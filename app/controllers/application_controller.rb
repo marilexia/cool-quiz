6e38a918c2f710c99cd31e5a7cd36bd5e2bde3ad
Marilexia Faith S Acelajado <faithacelajado@gmail.com>
2018-07-30 19:57:13 +0000

    cool quiz first commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,22 @@ class ApplicationController < Sinatra::Base
   get '/' do
     return erb :index
   end
+  
+    post  '/results' do
+   answers = params.values #"value" in index
+   @total= 0 #at the beginning of the quiz
+   answers.each do |answer| #goes to the array of values
+     @total += answer.to_i #adds up numbers that user chose -- also convert to integer bc we're taking in numbers
+   end
+   puts @total
+
+   @combo = cool_generator(@total) #refers to the method cool generator we defined in samplemodel 
+                                  #-- @total is the argument the method takes in
+   if @combo == "not_cool"
+     erb :not_cool
+   elsif @combo == "cool"
+     erb :cool
+  end
+
+end
 end
--- app/models/sample_model.rb
+++ app/models/sample_model.rb
@@ -1,3 +1,13 @@
-class SampleModel
-  attr_accessor :name, :age
+
+def cool_generator(user_value)
+     if user_value < 15
+       return "not_cool" #refers to the page not_cool.erb
+    elsif user_value == 22
+       return "cool"
+    elsif user_value <= 24
+       return "not_cool"
+      
+      end
+    
+    
 end
